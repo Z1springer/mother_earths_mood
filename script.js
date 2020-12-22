0f149127163edb23a2babac655d69e8991818e24
Zach <zdspringer99@gmail.com>
2020-12-21 22:22:51 -0800

    Edits on JS

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // console.log("Here be linked")
 
-
 // GIVEN a weather dashboard with form inputs
 // WHEN I search for a city
+
 var cityName = "";
 
 $("#log").on("click", function (event) {
@@ -11,9 +11,12 @@ $("#log").on("click", function (event) {
     // console.log(cityName);
     loadData(cityName)
 })
+
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
 
 function loadData(x) {
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + x + "&units=imperial&appid=ce311efbdbffb6909509e8536494ecba";
@@ -30,9 +33,9 @@ function loadData(x) {
             $.ajax({
                 url: coordsURL,
                 method: "GET"
-            }).then(function (answer) {
+            }).then(function (answer) {// WHEN I view future weather conditions for that city
                 console.log(answer);
-                for (var i = 0; i < 5; i++) {
+                for (var i = 0; i < 5; i++) {// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
                     $("#title" + i).text(dateFormat(parseInt(answer.daily[i].dt)))
                     $("#weatherIcon" + i).attr("src", `http://openweathermap.org/img/wn/${answer.daily[i].weather[0].icon}@4x.png`)
                     $("#temp" + i).text(answer.daily[i].temp.day + " °F")
@@ -42,11 +45,10 @@ function loadData(x) {
                 $(".current-uv").text(`UV: ${answer.current.uvi}`)
             })
             $("#city-date").text(`City Name: ${response.name}`);
-            $(".current-temp").text(`Temperature: ${response.main.temp} degrees F`);
+            $(".current-temp").text(`Temperature: ${response.main.temp} °F`);
             $(".current-hum").text(`Humidity: ${response.main.humidity}%`);
             $(".current-wind").text(`Wind Speed: ${response.wind.speed}`);
             $("#weatherIcon").attr("src", `http://openweathermap.org/img/wn/${response.weather[0].icon}@4x.png`)
-
         })
 }
 
@@ -58,31 +60,26 @@ function dateFormat(sequence) {
     var year = myDate.getFullYear();
     return month + "/" + day + "/" + year
 }
-    // WHEN I view the UV index
-    // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-    // WHEN I view future weather conditions for that city
-    // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
 
-    // WHEN I click on a city in the search history
-    // THEN I am again presented with current and future conditions for that city
-    // WHEN I open the weather dashboard
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+// WHEN I open the weather dashboard
+// THEN I am presented with the last searched city forecast
 
+var searchHist = []
 
-    // THEN I am presented with the last searched city forecast
-    // var searchHist = []
-
-    // document.querySelector("#search-city").addEventListener("submit", function (event) {
-    //     event.preventDefault();
-    //     var city = document.querySelector("#city").value
-    //     // console.log(city)
-    //     searchHist.push(city)
-    //     // console.log(searchHist)
-    //     localStorage.setItem('latest-city', city)
-    //     document.querySelector("#search-history").innerHTML = ""
-    //     for (i = 0; i < searchHist.length; i++) {
-    //         console.log(searchHist[i])
-    //         var div = document.createElement("div")
-    //         div.textContent = searchHist[i]
-    //         document.querySelector("#search-history").appendChild(div)
-    //     }
-    // })
+document.querySelector("#search-city").addEventListener("submit", function (event) {
+    event.preventDefault();
+    var city = document.querySelector("#city").value
+    // console.log(city)
+    searchHist.push(city)
+    // console.log(searchHist)
+    localStorage.setItem('latest-city', city)
+    document.querySelector("#search-history").innerHTML = ""
+    for (i = 0; i < searchHist.length; i++) {
+        console.log(searchHist[i])
+        var div = document.createElement("div")
+        div.textContent = searchHist[i]
+        document.querySelector("#search-history").appendChild(div)
+    }
+})
